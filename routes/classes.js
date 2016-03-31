ad7c195713b407cbc944e1160643459e142830e6
NguyenQuocHung-K58CA <qhungnb@gmail.com>
2016-03-31 23:56:09 +0700

    version 1.0.0

--- routes/classes.js
+++ routes/classes.js
@@ -25,4 +25,33 @@ router.get('/:id', function(req, res, next){
 	})
 });
 
+router.get('/:id/lessons', function(req, res, next){
+	Class.getClassesById([req.params.id], function(err, classDetail){
+		if (err) {
+			console.log(err);
+			res.send(err);
+		} else {
+			res.render('classes/lessons', {"class": classDetail });
+		}
+	})
+});
+
+router.get('/:id/lessons/:lesson_id', function(req, res, next){
+	Class.getClassesById([req.params.id], function(err, classDetail){
+		if (err) {
+			console.log(err);
+			res.send(err);
+		} else {			
+			lesson = {
+				lesson_title : "Could not find this lesson"
+			};
+			for (i=0; i<classDetail.lessons.length; i++)
+				if (classDetail.lessons[i].lesson_number == req.params.lesson_id) {
+					lesson = classDetail.lessons[i];
+				}
+			res.render('classes/lesson', {"class": classDetail, "lesson":lesson });
+		}
+	})
+});
+
 module.exports = router;
