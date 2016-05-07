dbc7439da34d06691c498fa4ece41f94899e7b18
nts <ngotungson0602@gmail.com>
2016-05-07 16:03:27 +0700

    remove user display on header when not login

--- routes/users.js
+++ routes/users.js
@@ -51,7 +51,7 @@ router.post('/signup', function(req, res, next){
 		console.log(err);
 	}
 
-	if (errors){		
+	if (errors){
 		res.render('users/signup', {
 			errors: errors,
 			first_name: first_name,
@@ -70,11 +70,11 @@ router.post('/signup', function(req, res, next){
 			email: email,
 			username: username,
 			password: password,
-			avatar_url: '/images/stevie.jpg',
+			avatar_url: '/images/default_avatar.png',
 			type: type
 		});
 
-		if (type == 'student'){	
+		if (type == 'student'){
 			var newStudent = new Student({
 				first_name: first_name,
 				last_name: last_name,
@@ -165,7 +165,7 @@ passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
   });
-}); 
+});
 
 passport.use(new LocalStrategy(
 	function(username, password, done) {
@@ -202,7 +202,7 @@ passport.use(new RememberMeStrategy(
     consumeRememberMeToken(token, function(err, uid) {
       if (err) { return done(err); }
       if (!uid) { return done(null, false); }
-      
+
       findById(uid, function(err, user) {
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
@@ -211,7 +211,7 @@ passport.use(new RememberMeStrategy(
     });
   },
   issueToken
-)); 
+));
 
 function issueToken(user, done) {
   var token = utils.randomString(64);
@@ -221,12 +221,12 @@ function issueToken(user, done) {
   });
 }
 
-router.post('/login', 
+router.post('/login',
   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
   function(req, res, next) {
     // Issue a remember me cookie if the option was checked
     if (!req.body.remember_me) { return next(); }
-    
+
     issueToken(req.user, function(err, token) {
       if (err) { return next(err); }
       res.cookie('remember_me', token, { path: '/', httpOnly: true, maxAge: 604800000 });
@@ -241,7 +241,7 @@ router.post('/login',
  	passport.authenticate('local', {
     	failureRedirect: '/',
     	failureFlash: 'Invalid username or password'
-	}), 
+	}),
 	function(req, res, next) {
 		console.log('Authentication Successful');
 		req.flash('success', 'You are logged in ');
