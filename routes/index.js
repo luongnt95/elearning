a540d3ef2c5b6be3b57b04ee6cb645197bc0dc62
luongnt95 <luongnt.58@gmail.com>
2016-06-16 19:14:44 +0700

    fix rating

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,29 @@ router.get('/', function(req, res, next) {
 			console.log(err);
 			res.send(err);
 		} else {
-  			res.render('index', { "classes": classes, "isHome": true, "messages": req.flash('success')});
+			var ratingScores = [];
+			for(i in classes) {
+				var klass = classes[i];
+				Rating.find({class_id: klass.id}, function(err, ratings) {
+					var sum = 0;
+					var len = ratings.length;					
+					for(var i = 0; i < len; i++) {
+						sum += ratings[i].score;
+					}
+					if(len == 0) {
+						ratingScores.push(0);
+					}
+					else {
+						ratingScores.push(Math.round(sum/len));
+					}
+					if(ratingScores.length == classes.length) {
+						for(var index in classes) {
+							classes[index].ratingScore = ratingScores[index];
+						}
+						res.render('index', { "classes": classes, "isHome": true, "messages": req.flash('success')});
+					}
+				});
+			}
 		}
 	}, 3);
 });
