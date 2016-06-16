afa4d416f1957cc5640448c9f4bcb587d83c9575
luongnt95 <luongnt.58@gmail.com>
2016-06-17 01:20:48 +0700

    fail to deploy

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res, next) {
 							for(var index in classes) {
 								classes[index].ratingScore = ratingScores[index];
 							}
-							res.render('index', { "classes": classes, "isHome": true, "messages": req.flash('success')});
+							res.render('index', { "classes": classes, "isHome": true});
 						}
 					});					
 				})(i);
